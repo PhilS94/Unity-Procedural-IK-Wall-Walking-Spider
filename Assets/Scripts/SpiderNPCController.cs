1844b02c6bf48adec8a7c5090e35bf3361936d8d
PhilS94 <PhilS94@users.noreply.github.com>
2020-02-20 23:45:28 +0100

    Fixed walk speed of NPCController, Added debug drawing

--- Assets/Scripts/SpiderNPCController.cs
+++ Assets/Scripts/SpiderNPCController.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpiderNPCController : MonoBehaviour {
+
+
+    [Header("Debug")]
+    public bool showDebug;
+
+    [Header("Spider Reference")]
     public Spider spider;
+
     private float perlinStepDirection = 0.07f;
     private float perlinStepSpeed = 0.2f;
     private float startValue;
@@ -21,28 +28,32 @@ public class SpiderNPCController : MonoBehaviour {
         Y = transform.up;
     }
 
-    // Update is called once per frame
-    void Update() {
-
-
-    }
-
     private void FixedUpdate() {
         Vector3 input = getDirection();
-        float speed = getSpeed(0, spider.walkSpeed);
+        float speed = getSpeed(0, 1);
         spider.walk(input, speed * Time.fixedDeltaTime);
         spider.turn(input, speed * Time.fixedDeltaTime);
 
         //Debug.DrawLine(spider.transform.position, spider.transform.position + input * 5.0f, Color.cyan);
     }
 
+    private void Update() {
+        if (showDebug) {
+            Debug.DrawLine(spider.transform.position, spider.transform.position + getVectorInThisCoordinateSystem(X) * 0.5f * spider.scale, Color.red);
+            Debug.DrawLine(spider.transform.position, spider.transform.position + getVectorInThisCoordinateSystem(Z) * 0.5f * spider.scale, Color.blue);
+        }
+    }
     private Vector3 getDirection() {
         float vertical = 2.0f * (Mathf.PerlinNoise(Time.time * perlinStepDirection, startValue) - 0.5f); // Range [-1,1]
         float horizontal = 2.0f * (Mathf.PerlinNoise(Time.time * perlinStepDirection, startValue + 0.3f) - 0.5f); // Range [-1,1]
-        return (Quaternion.FromToRotation(Y, spider.getGroundNormal()) * (X * horizontal + Z * vertical)).normalized;
+        return (getVectorInThisCoordinateSystem((X * horizontal + Z * vertical).normalized));
     }
 
     private float getSpeed(float min, float max) {
         return min + Mathf.PerlinNoise(Time.time * perlinStepSpeed, startValue + 0.6f) * (max - min);// Range [min,max]
+    }
+
+    private Vector3 getVectorInThisCoordinateSystem(Vector3 v) {
+        return Quaternion.FromToRotation(Y, spider.getGroundNormal()) * v;
     }
 }
