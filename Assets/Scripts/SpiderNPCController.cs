3c5dd1a425424e6983d03dbddf1ea0f3e004a5fa
PhilS94 <PhilS94@users.noreply.github.com>
2020-02-27 23:15:21 +0100

    Uncommented speed variable for SpiderNPCController

--- Assets/Scripts/SpiderNPCController.cs
+++ Assets/Scripts/SpiderNPCController.cs
@@ -31,8 +31,8 @@ public class SpiderNPCController : MonoBehaviour {
     private void FixedUpdate() {
         Vector3 input = getDirection();
         float speed = getSpeed(0, 1);
-        spider.walk(speed * input);
-        spider.turn(input, speed);
+        spider.walk(input);
+        spider.turn(input);
 
         //Debug.DrawLine(spider.transform.position, spider.transform.position + input * 5.0f, Color.cyan);
     }
