acbecabe5f4da9c3ddff66f05e58001d0804f043
PhilS94 <PhilS94@users.noreply.github.com>
2020-03-30 22:43:57 +0200

    Added abstract camera class from which the smooth and spectator cam inherit
    SmoothCamera and SpectatingCamera both inherit from the abstract class CameraAbstract.
    
    While SmoothCamera is the cam used to control the spider, that is the controlling camera.
    The Spectating cam is used for the random walk which does not rotate with the spider unlike the above camera.

--- Assets/Scripts/SmoothCamera.cs
+++ Assets/Scripts/SmoothCamera.cs
@@ -5,33 +5,11 @@ using UnityEngine.Rendering;
 using Raycasting;
 
 [RequireComponent(typeof(Camera))]
-public class SmoothCamera : MonoBehaviour {
+public class SmoothCamera : CameraAbstract {
 
-    public bool showDebug;
-    public Transform parent;
-
-    private Transform camTarget;
-    private Camera cam;
-
-    [Header("Smoothness")]
-    public bool followPosition;
-    public bool followRotation;
-    public float translationSpeed;
-    public float rotationSpeed;
+    [Header("Camera Roll Damp")]
     [Range(0, 1)]
-    public float parentNormalCounterAdjust;
-
-    [Header("Sensitivity")]
-    [Range(1, 5)]
-    public float XSensitivity;
-    [Range(1, 5)]
-    public float YSensitivity;
-
-    [Header("Angle Restrictions")]
-    [Range(0.01f, 90.0f)]
-    public float camUpperAngleMargin = 30.0f;
-    [Range(0.01f, 90.0f)]
-    public float camLowerAngleMargin = 60.0f;
+    public float rollDamp;
 
     [Header("Camera Clipping")]
     public bool hideObstructions;
@@ -53,86 +31,53 @@ public class SmoothCamera : MonoBehaviour {
 
     private Vector3 lastParentNormal;
 
-    void Awake() {
-
-        cam = GetComponent<Camera>();
-        setupCamTarget();
-
-        // Now unparent the camera itself so it can move freely and use the above target to lerp
-        transform.parent = null;
-
+    new void Awake() {
+        base.Awake();
         initializeRayCasting();
-        lastParentNormal = parent.up;
-    }
-
-    private void setupCamTarget() {
-        // For the target, create new Gameobject with same position and rotation as camera starts with and parent to Spider
-        GameObject g = new GameObject("SmoothCamera Target");
-        camTarget = g.transform;
-        camTarget.position = transform.position;
-        camTarget.rotation = transform.rotation;
-        camTarget.parent = parent;
+        lastParentNormal = observedObject.up;
+        camTarget.parent = observedObject;
     }
 
     private void initializeRayCasting() {
-        maxCameraDistance = Vector3.Distance(parent.position, transform.position);
-        playerToCam = new RayCast(parent.position, camTarget.position, parent, null);
-        camToPlayer = new SphereCast(transform.position, parent.position, rayRadiusForObstructions * transform.lossyScale.y * 0.01f, transform, parent);
+        maxCameraDistance = Vector3.Distance(observedObject.position, transform.position);
+        playerToCam = new RayCast(observedObject.position, camTarget.position, observedObject, null);
+        camToPlayer = new SphereCast(transform.position, observedObject.position, rayRadiusForObstructions * transform.lossyScale.y * 0.01f, transform, observedObject);
     }
 
-    private void Update() {
-        if (!cam.enabled) return;
+    new void Update() {
+        base.Update();
 
-        if (parentNormalCounterAdjust != 0) {
-            float angle = Vector3.SignedAngle(lastParentNormal, parent.up, camTarget.right);
-            RotateCameraVertical(parentNormalCounterAdjust * -angle);
-            lastParentNormal = parent.up;
+        if (rollDamp != 0) {
+            float angle = Vector3.SignedAngle(lastParentNormal, observedObject.up, camTarget.right);
+            RotateCameraVertical(rollDamp * -angle);
+            lastParentNormal = observedObject.up;
         }
 
-        RotateCameraHorizontal(Input.GetAxis("Mouse X") * XSensitivity, false);
-        RotateCameraVertical(-Input.GetAxis("Mouse Y") * YSensitivity, false);
+        if (!cam.enabled) return;
         if (clipToLevelBorder) clipCamera();
         if (hideObstructions) clipCameraInvisible();
     }
 
-    private void LateUpdate() {
-        /* Now do the lerping of the actual camera to the target for the smooth movement */
+    new void LateUpdate() {
+        base.LateUpdate();
 
-        // Translation Slerping
-        if (followPosition) {
-            Vector3 a = parent.InverseTransformPoint(transform.position);
-            Vector3 b = parent.InverseTransformPoint(camTarget.position);
-            Vector3 c = Vector3.Slerp(a, b, Time.deltaTime * translationSpeed);
-            transform.position = parent.TransformPoint(c);
-        }
-
-
-        //Rotation Slerping
-        if (followRotation) {
-            //transform.position = Vector3.SmoothDamp(transform.position, camTarget.position, ref velocity, lerpTime);
-            transform.rotation = Quaternion.Slerp(transform.rotation, camTarget.rotation, Time.deltaTime * rotationSpeed);
-        }
-        if (showDebug) drawDebug();
+        // Rotation Interpolation
+        transform.rotation = Quaternion.Slerp(transform.rotation, camTarget.rotation, Time.deltaTime * rotationSpeed);
     }
 
-    public void RotateCameraHorizontal(float angle, bool onlyTarget = true) {
+    public override void RotateCameraHorizontal(float angle, bool onlyTarget = true) {
         if (angle == 0) return;
-        camTarget.RotateAround(parent.position, parent.transform.up, angle);
-        if (!onlyTarget) transform.RotateAround(parent.position, parent.transform.up, angle);
+        camTarget.RotateAround(observedObject.position, observedObject.transform.up, angle);
+        if (!onlyTarget) transform.RotateAround(observedObject.position, observedObject.transform.up, angle);
     }
 
-    public void RotateCameraVertical(float angle, bool onlyTarget = true) {
+    public override void RotateCameraVertical(float angle, bool onlyTarget = true) {
 
-        angle = angle % 360;
-        if (angle == -180) angle = 180;
-        if (angle > 180) angle -= 360;
-        if (angle < -180) angle += 360;
-        //Now angle is of the form (-180,180]
-
+        clampAngle(ref angle);
         if (angle == 0) return;
 
         // Adjust angle according to upper and lower bound
-        float currentAngle = Vector3.SignedAngle(parent.transform.up, parent.transform.position - camTarget.position, camTarget.right); //Should always be positive
+        float currentAngle = Vector3.SignedAngle(observedObject.transform.up, observedObject.transform.position - camTarget.position, camTarget.right); //Should always be positive
         if (currentAngle + angle < camLowerAngleMargin) {
             angle = camLowerAngleMargin - currentAngle;
         }
@@ -141,8 +86,8 @@ public class SmoothCamera : MonoBehaviour {
         }
 
         // Apply Rotation
-        camTarget.RotateAround(parent.position, camTarget.right, angle);
-        if (!onlyTarget) transform.RotateAround(parent.position, camTarget.right, angle);
+        camTarget.RotateAround(observedObject.position, camTarget.right, angle);
+        if (!onlyTarget) transform.RotateAround(observedObject.position, camTarget.right, angle);
     }
 
     void clipCamera() {
@@ -150,7 +95,7 @@ public class SmoothCamera : MonoBehaviour {
         playerToCam.setDistance(maxCameraDistance);
 
         if (playerToCam.castRay(out hitInfo, cameraClipLayer)) {
-            float margin = Mathf.Min(cameraClipMargin, 0.9f * Vector3.Distance(hitInfo.point, parent.position));
+            float margin = Mathf.Min(cameraClipMargin, 0.9f * Vector3.Distance(hitInfo.point, observedObject.position));
             setTargetPosition(hitInfo.point - margin * playerToCam.getDirection());
         }
         else setTargetPosition(playerToCam.getEnd());
@@ -179,64 +124,8 @@ public class SmoothCamera : MonoBehaviour {
         }
     }
 
-    public Camera getCamera() {
-        return cam;
-    }
-
-    public Transform getCameraTarget() {
-        return camTarget;
-    }
-
-    public Vector3 getCamTargetPosition() {
-        return camTarget.position;
-    }
-
-    public Quaternion getCamTargetRotation() {
-        return camTarget.rotation;
-    }
-
-    public Transform getTargetParent() {
-        return parent;
-    }
-
-    public void setTargetPosition(Vector3 pos) {
-        camTarget.position = pos;
-    }
-
-    public void setTargetRotation(Quaternion rot) {
-        camTarget.rotation = rot;
-    }
-
-    //** Debug Methods **//
-    private void drawDebug() {
-        camToPlayer.draw(Color.white);
+    new void drawDebug() {
+        base.drawDebug();
         DebugShapes.DrawRay(camToPlayer.getOrigin(), cameraClipMargin * camToPlayer.getDirection(), Color.black);
-
-        //Something buggy here
-        float currentAngle = Vector3.SignedAngle(parent.transform.up, parent.transform.position - camTarget.position, camTarget.right);
-        Vector3 v = Quaternion.AngleAxis(-currentAngle, camTarget.right) * camToPlayer.getDirection();
-        Vector3 up = parent.TransformDirection(Quaternion.AngleAxis(camUpperAngleMargin, camTarget.right) * v);
-        Vector3 down = parent.TransformDirection(Quaternion.AngleAxis(camLowerAngleMargin, camTarget.right) * v);
-        DebugShapes.DrawRay(parent.transform.position, up, Color.black);
-        DebugShapes.DrawRay(parent.transform.position, down, Color.black);
-
-        DebugShapes.DrawPoint(camTarget.position, Color.magenta, 0.1f);
-        DebugShapes.DrawRay(camTarget.position, camTarget.forward, Color.blue);
-        DebugShapes.DrawRay(camTarget.position, camTarget.right, Color.red);
-        DebugShapes.DrawRay(camTarget.position, camTarget.up, Color.green);
-
-
     }
-
-#if UNITY_EDITOR
-    void OnDrawGizmosSelected() {
-        if (!showDebug) return;
-        if (UnityEditor.EditorApplication.isPlaying) return;
-        if (!UnityEditor.Selection.Contains(transform.gameObject)) return;
-        if (parent == null) return;
-        camTarget = transform;
-        initializeRayCasting();
-        drawDebug();
-    }
-#endif
 }
