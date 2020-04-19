5988e93c001f1849b95636e7e1adb645f5c27fb6
Phil S <p.scho94@gmail.com>
2020-04-20 00:02:31 +0200

    Added scene reset with esc key press

--- Assets/Scripts/GeneralSettings.cs
+++ Assets/Scripts/GeneralSettings.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 /* Simple class for some general game settings. */
 
 public class GeneralSettings : MonoBehaviour {
     private void Awake() {
+        // Lock Cursor in Build
         Cursor.lockState = CursorLockMode.Locked;
+
+        //Unlock Cursor in Editor
 #if UNITY_EDITOR
         Cursor.lockState = CursorLockMode.None;
 #endif
+    }
 
+    private void Update() {
+        //On Press reset scene
+        if (Input.GetKeyDown(KeyCode.Escape)) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
